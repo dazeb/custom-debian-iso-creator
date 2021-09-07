af7d1646fdede1f00ed457b6a92fff267a6f0c6e
Will <willhy@gmail.com>
2021-09-07 16:00:13 -0500

    feat(cache): updates to allow for apt-cacher-ng
    Only used in debootstrap then removed for runtime

--- lib/generate.py
+++ lib/generate.py
@@ -8,17 +8,28 @@ import yaml
 with open("/app/config.yaml", "r") as config_file:
   config = yaml.safe_load(config_file)
 
-os.environ["CONFIG_USER"] = config["user"]["name"]
-os.environ["CONFIG_PASSWORD"] = config["user"]["password"]
-os.environ["CONFIG_RELEASE"] = config["debian"]["release"]
-os.environ["CONFIG_PACKAGES_STANDARD"] = config["debian"]["packages"]["standard"]
-os.environ["CONFIG_PACKAGES_DOWNLOAD_ONLY"] = config["debian"]["packages"]["download_only"]
-os.environ["CONFIG_PACKAGES_BACKPORTS"] = config["debian"]["packages"]["backports"]
-os.environ["CONFIG_COMMANDS_CHROOT"] = config["commands"]["chroot"]
-os.environ["CONFIG_APT_SERVER_ADDRESS"] = config["debian"]["apt"]["server_address"]
-os.environ["CONFIG_APT_LIST_BACKPORTS"] = config["debian"]["apt"]["lists"]["backports"]
-os.environ["CONFIG_APT_LIST_EXTRAS"] = config["debian"]["apt"]["lists"]["extras"]
-os.environ["CONFIG_APT_LIST_SECURITY"] = config["debian"]["apt"]["lists"]["security"]
+user = config.get("user", {})
+debian = config.get("debian", {})
+debian_apt = debian.get("apt", {})
+debian_apt_lists = debian_apt.get("lists", {})
+debian_apt_packages = debian_apt.get("packages", {})
+commands = config.get("commands", {})
+
+# Required config settings.
+os.environ["CONFIG_USER"] = user.get("name")
+os.environ["CONFIG_PASSWORD"] = user.get("password")
+os.environ["CONFIG_RELEASE"] = debian.get("release")
+os.environ["CONFIG_APT_SERVER_ADDRESS"] = debian_apt.get("server_address")
+
+# Optional config settings.
+os.environ["CONFIG_PACKAGES_STANDARD"] = debian_apt_packages.get("standard", "")
+os.environ["CONFIG_PACKAGES_BACKPORTS"] = debian_apt_packages.get("backports", "")
+os.environ["CONFIG_PACKAGES_DOWNLOAD_ONLY"] = debian_apt_packages.get("download_only", "")
+os.environ["CONFIG_COMMANDS_CHROOT"] = commands.get("chroot", "")
+os.environ["CONFIG_APT_CACHE_SERVER_ADDRESS"] = debian_apt.get("bootstrap_cache_server_address", "")
+os.environ["CONFIG_APT_LIST_BACKPORTS"] = debian_apt_lists.get("backports", "")
+os.environ["CONFIG_APT_LIST_EXTRAS"] = debian_apt_lists.get("extras", "")
+os.environ["CONFIG_APT_LIST_SECURITY"] = debian_apt_lists.get("security", "")
 
 subprocess.run("/app/scripts/_bootstrap_debian.sh", check=True, shell=True, stdout=sys.stdout, stderr=sys.stderr)
 subprocess.run("/app/scripts/_customize_debian.sh", check=True, shell=True, stdout=sys.stdout, stderr=sys.stderr)
--- lib/scripts/_bootstrap_debian.sh
+++ lib/scripts/_bootstrap_debian.sh
@@ -4,9 +4,20 @@ set -e
 
 rm -rf "${HOME}/LIVE_BOOT/chroot"
 
+cache_server=""
+if [ -n "${CONFIG_APT_CACHE_SERVER_ADDRESS}" ];
+then
+  cache_server="${CONFIG_APT_CACHE_SERVER_ADDRESS}/"
+fi
+
 debootstrap \
   --arch=amd64 \
   --variant=minbase \
   "${CONFIG_RELEASE}" \
   "${HOME}/LIVE_BOOT/chroot" \
-  "http://${CONFIG_APT_SERVER_ADDRESS}/debian/"
+  "http://${cache_server}${CONFIG_APT_SERVER_ADDRESS}/debian/"
+
+if [ -n "${cache_server}" ];
+then
+  sed -i "s|${cache_server}||g" ${HOME}/LIVE_BOOT/chroot/etc/apt/*.list
+fi
